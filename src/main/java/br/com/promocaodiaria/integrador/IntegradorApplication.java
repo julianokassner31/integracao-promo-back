6960d23cffb2c7813caf896f97cb10f92ba59894
Juliano Kassner <juliano.kassner@ciss.com.br>
2020-09-06 19:38:41 -0300

    Ajuste para quando dar refresh na pagina nao dar erro.

--- src/main/java/br/com/promocaodiaria/integrador/IntegradorApplication.java
+++ src/main/java/br/com/promocaodiaria/integrador/IntegradorApplication.java
@@ -2,17 +2,31 @@ package br.com.promocaodiaria.integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableWebMvc
-public class IntegradorApplication {
+@Controller
+public class IntegradorApplication implements ErrorController {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(IntegradorApplication.class, args);
 		
 	}
 
+	private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "forward:/index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+    
 }
