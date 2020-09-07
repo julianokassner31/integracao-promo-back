0e00f264b67ac05264d0644eddf299daa00368a6
Juliano Kassner <juliano.kassner@ciss.com.br>
2020-09-06 21:32:40 -0300

    ajuste response

--- src/main/java/br/com/promocaodiaria/integrador/controller/ConfigController.java
+++ src/main/java/br/com/promocaodiaria/integrador/controller/ConfigController.java
@@ -84,11 +84,11 @@ public class ConfigController {
 			
 			} else {
 				
-				return ResponseEntity.notFound().build();
+				return ResponseEntity.badRequest().build();
 			}
 		}
 		
-		return ResponseEntity.badRequest().build();
+		return ResponseEntity.notFound().build();
 	}
 	
 	@GetMapping("sistemas")
