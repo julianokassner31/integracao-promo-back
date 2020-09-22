b90c8376d4265d5120771e1907bd7c579d6f156b
Juliano Kassner <juliano.kassner@ciss.com.br>
2020-09-22 19:02:13 -0300

    Ajuste webconfig

--- src/main/java/br/com/promocaodiaria/integrador/config/MvcConfigurer.java
+++ src/main/java/br/com/promocaodiaria/integrador/config/MvcConfigurer.java
@@ -1,10 +1,14 @@
 package br.com.promocaodiaria.integrador.config;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class MvcConfigurer implements WebMvcConfigurer {
